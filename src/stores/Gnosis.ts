68f4c3154ebcff32974df9f3966798dd3d2b128e
Tom French <tom@tomfren.ch>
2020-08-05 15:26:36 +0100

    fix: update gnosis store to use etherv5 encoding syntax

--- src/stores/Gnosis.ts
+++ src/stores/Gnosis.ts
@@ -1,15 +1,14 @@
 import { observable, action } from 'mobx';
-import { Interface } from 'ethers/utils';
+import { Interface } from '@ethersproject/abi';
+import initSdk, { SdkInstance, SafeInfo } from '@gnosis.pm/safe-apps-sdk';
 import RootStore from './Root';
 import { ContractTypes, schema } from './Provider';
-import initSdk, { SdkInstance, SafeInfo } from "@gnosis.pm/safe-apps-sdk";
-
 
 export type Transaction = {
-    data: string,
-    to: string,
-    value: number
-}
+    data: string;
+    to: string;
+    value: number;
+};
 
 export default class Gnosis {
     @observable safeAddress: string;
@@ -19,21 +18,21 @@ export default class Gnosis {
 
     constructor(rootStore) {
         this.rootStore = rootStore;
-        this.network = "";
-        this.safeAddress = "";
-        this.sdkInstance = initSdk()
+        this.network = '';
+        this.safeAddress = '';
+        this.sdkInstance = initSdk();
 
-        this.setSafeInfo = this.setSafeInfo.bind(this)
+        this.setSafeInfo = this.setSafeInfo.bind(this);
 
         this.sdkInstance.addListeners({
             onSafeInfo: this.setSafeInfo,
         });
     }
 
-    setSafeInfo(safeInfo: SafeInfo):void {
-        console.log("Setting account to ", safeInfo.safeAddress)
+    setSafeInfo(safeInfo: SafeInfo): void {
+        console.log('Setting account to ', safeInfo.safeAddress);
         this.safeAddress = safeInfo.safeAddress;
-        this.network = safeInfo.network
+        this.network = safeInfo.network;
     }
 
     getInstanceAddress = (): string => {
@@ -56,17 +55,15 @@ export default class Gnosis {
 
         const transaction = {
             to: contractAddress,
-            data: iface.functions[action].encode(params),
-            value: value || 0
-        }
+            data: iface.encodeFunctionData(action, params),
+            value: value || 0,
+        };
         return transaction;
     }
 
-    @action sendTransaction = (
-        transaction: Transaction
-    ): void => this.sendTransactions([transaction]);
+    @action sendTransaction = (transaction: Transaction): void =>
+        this.sendTransactions([transaction]);
 
-    @action sendTransactions = (
-        transactions: Transaction[]
-    ): void => this.sdkInstance.sendTransactions(transactions);
+    @action sendTransactions = (transactions: Transaction[]): void =>
+        this.sdkInstance.sendTransactions(transactions);
 }
